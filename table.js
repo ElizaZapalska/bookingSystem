0721bda2ebf1f4ad77ccb7724c592459001cbfa6
Eliza Zapalska <zapalska.eliza@gmail.com>
2020-08-16 17:04:57 +0200

    add atributes to the empty cells

--- table.js
+++ table.js
@@ -1,6 +1,4 @@
 
-
-
 function generateTable(hours, classrooms) {
 
     appendHours(hours);
@@ -26,17 +24,28 @@ function appendClassrooms(classrooms, hours) {
         let classNumber = document.createElement("td");
         classNumber.innerText = classroom
         newRow.appendChild(classNumber)
-        addEmptyCells(newRow, hours);
+        addEmptyCells(newRow, hours, classroom);
         table.appendChild(newRow)
     }
 }
 
-function addEmptyCells(newRow, hours) {
+function addEmptyCells(newRow, hours, classroom) {
     let i = 0;
     while (i < hours.length - 1) {
         let td = document.createElement("td");
+        td.setAttribute("hour", hours[i+1])
+        td.setAttribute("classroom", classroom)
+        td.onclick = () => markAsBooked(td);
         newRow.appendChild(td)
         i += 1;
+    }
+}
+
+function markAsBooked(td) {
+    if (td.classList.contains("booked")) {
+        td.classList.remove("booked")
+    } else {
+        td.setAttribute("class", "booked")
     }
 }
 
